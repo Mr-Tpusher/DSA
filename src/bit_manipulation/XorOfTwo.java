6fa200ad75ff3d88c387845d7762e0d2efcd0c8f
Mr.Tpusher <tech.tpusher@gmail.com>
2024-09-05 01:19:59 +0530

    add bit manipulation problems

---
+++ src/bit_manipulation/XorOfTwo.java
@@ -0,0 +1,34 @@
+package bit_manipulation;
+/*
+ * Given an array, Find two such elements
+ * whose XOR is K
+ */
+
+import java.util.*;
+public class XorOfTwo {
+    public static void main(String[] args) {
+        int[] A = {1, 5, 8, 3, 11};
+        int k = 11;
+
+        // A ^ B = k i.e. A ^ A ^ B = A ^ K
+        // B = A ^ K
+
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            hs.add(A[i]);
+        }
+
+        int first, second;
+        first = second = 0;
+        for (int i = 0; i < A.length; i++) {
+            int B = A[i] ^ k;
+            if (hs.contains(B)) {
+                break;
+            }
+            first = A[i];
+            second = B;
+        }
+
+        System.out.println( first + "," + second);
+    }
+}
