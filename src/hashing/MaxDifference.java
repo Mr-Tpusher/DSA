32cab8a54a79d9c909c31f536ca7926bb551746a
Mr-Tpusher <tech.tpusher@gmail.com>
2024-09-27 00:37:42 +0530

    Add maxDiffUsingTwoHashmaps

--- src/hashing/MaxDifference.java
+++ src/hashing/MaxDifference.java
@@ -1,4 +1,8 @@
 package hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 * Given an array of integers, find Max(abs|i-j|)
 * such that A[i]==A[j] and i!=j
@@ -7,6 +11,7 @@ public class MaxDifference {
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 2, 7, 6, 3, 2, 1};
         System.out.println(bruteForce(A));
+        System.out.println(maxDiffUsingTwoHashmaps(A));
 
     }
 
@@ -23,6 +28,29 @@ public class MaxDifference {
                 }
             }
             max = Math.max(max, currMax);
+        }
+        return max;
+    }
+
+    public static int maxDiffUsingTwoHashmaps(int[] A) {
+        if (A.length == 0) {
+            return -1;
+        }
+        HashMap<Integer, Integer> firstOccurrence = new HashMap<>();
+        HashMap<Integer, Integer> lastOccurrence = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            int curr = A[i];
+            if (! firstOccurrence.containsKey(A[i])) {
+                firstOccurrence.put(curr, i);
+            }
+            lastOccurrence.put(curr, i);
+        }
+
+        int max = 0;
+        for (Map.Entry<Integer, Integer> e: firstOccurrence.entrySet()) {
+            int first = e.getValue();
+            int last = lastOccurrence.get(e.getKey());
+            max = Math.max(max, last - first);
         }
         return max;
     }
