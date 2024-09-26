abd17e58a1cb23d57fb5e1067b841e5e3dcad67c
Mr-Tpusher <tech.tpusher@gmail.com>
2024-09-26 23:14:14 +0530

    Add ElementFrequency

---
+++ src/hashing/ElementFrequency.java
@@ -0,0 +1,81 @@
+package hashing;
+
+import java.util.Arrays;
+
+/*
+* Given an array and multiple queries, in each query you are given a number,
+* you have to tell how many times that number exists in the input array.
+*
+*
+* */
+public class ElementFrequency {
+    public static void main(String[] args) {
+        int[] A = {2, 3, 5, 3, 3, 1, 5};
+        int[] queries = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(bruteForce1(A, queries)));
+        System.out.println(Arrays.toString(bruteForce2(A, queries)));
+    }
+
+    public static int[] bruteForce1(int[] A, int[] queries) {
+        int[] output = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int frequency = 0;
+            for (int j = 0; j < A.length; j++) {
+                if (queries[i] == A[j]) {
+                    frequency++;
+                }
+            }
+            output[i] = frequency;
+        }
+        return output;
+    }
+
+    public static int[] bruteForce2(int[] A, int[] queries) {
+        Arrays.sort(A);
+        int[] output = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int queryElement = queries[i];
+            int firstOcc = getFirstOccurrence(A, queryElement);
+            int lastOcc = getLastOccurrence(A, queryElement);
+            int freq = firstOcc == -1 ? 0 : lastOcc - firstOcc + 1;
+            output[i] = freq;
+        }
+        return output;
+    }
+
+    public static int getFirstOccurrence(int[] A, int element) {
+        int firstOccurence = -1;
+        int left = 0, right = A.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = A[mid];
+            if (midElement == element) {
+                firstOccurence = mid;
+                right = mid - 1;
+            } else if (midElement < element) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return firstOccurence;
+    }
+
+    public static int getLastOccurrence(int[] A, int element) {
+        int lastOccurrence = -1;
+        int left = 0, right = A.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = A[mid];
+            if (midElement == element) {
+                lastOccurrence = mid;
+                left = mid + 1;
+            } else if (midElement < element) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return lastOccurrence;
+    }
+}
