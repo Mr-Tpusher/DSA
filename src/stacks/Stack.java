7f7bdeedb826f861e1f801db3c945f381462917a
Mr-Tpusher <tech.tpusher@gmail.com>
2024-10-03 17:14:14 +0530

    Add stack using ArrayList

---
+++ src/stacks/Stack.java
@@ -0,0 +1,37 @@
+package stacks;
+
+import java.util.ArrayList;
+
+public class Stack<T> {
+    private final ArrayList<T> elements;
+
+    Stack() {
+         elements = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public boolean push(T element) {
+        return elements.add(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Underflow!");
+        }
+        return elements.remove(elements.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Underflow!");
+        }
+        return elements.get(elements.size() - 1);
+    }
+
+    public String toString() {
+        return elements.toString();
+    }
+}
