6e46ec02a374ab205a1fb0c8b6489a4bc3cec475
Mr-Tpusher <tech.tpusher@gmail.com>
2024-10-06 11:23:05 +0530

    Largest Rectangle in Histogram

--- src/stacks/LargestRectangleInHistogram.java
+++ src/stacks/LargestRectangleInHistogram.java
@@ -9,6 +9,7 @@ public class LargestRectangleInHistogram {
     public static void main(String[] args) {
         int[] A = {6, 2, 5, 4, 5, 1, 6};
         System.out.println(bruteForce(A));
+        System.out.println(solve(A));
     }
 
     public static int bruteForce(int[] A) {
@@ -34,5 +35,53 @@ public class LargestRectangleInHistogram {
             maxArea = Math.max(maxArea, currArea);
         }
         return maxArea;
+    }
+
+    public static int solve(int[] A) {
+        int length = A.length;
+        int[] nearestSmallerOnLeft = getNearestSmallerOnLeft(A);
+        int[] nearestSmallerOnRight = getNearestSmallerOnRight(A);
+
+        int largestRectangle = 0;
+        for (int i = 0; i < length; i++) {
+            int totalBlocks = nearestSmallerOnRight[i] - nearestSmallerOnLeft[i] - 1;
+            int currRectangle = A[i] * totalBlocks;
+            largestRectangle = Math.max(largestRectangle, currRectangle);
+        }
+        return largestRectangle;
+    }
+
+    public static int[] getNearestSmallerOnLeft(int[] A) {
+        //A = {6, 2, 5, 4, 5, 1, 6};
+        int length = A.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] nearestSmallerOnLeft = new int[length];
+        for (int i = 0; i < length; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] >= A[i])
+                stack.pop();
+            if (stack.isEmpty())
+                nearestSmallerOnLeft[i] = -1;
+            else
+                nearestSmallerOnLeft[i] = stack.peek();
+            stack.push(i);
+        }
+        return nearestSmallerOnLeft;
+    }
+
+    public static int[] getNearestSmallerOnRight(int[] A) {
+        //A = {6, 2, 5, 4, 5, 1, 6};
+        int length = A.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] nearestSmallerOnRight = new int[length];
+        for (int i = length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && A[stack.peek()] >= A[i])
+                stack.pop();
+            if (stack.isEmpty())
+                nearestSmallerOnRight[i] = length;
+            else
+                nearestSmallerOnRight[i] = stack.peek();
+            stack.push(i);
+        }
+        return nearestSmallerOnRight;
     }
 }
