56d8e2d4f056f4aadca420f83efcdfe9e9bac390
Mr-Tpusher <tech.tpusher@gmail.com>
2024-10-20 19:12:42 +0530

    revise binary search

--- src/revision/BinarySearch.java
+++ src/revision/BinarySearch.java
@@ -3,11 +3,13 @@ package revision;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] A = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println(binarySearch(A, 0));
+        int k = 5;
+        System.out.println(binarySearchIterative(A, k));
+        System.out.println(binarySearchRecursive(A, k, 0 , A.length - 1));
 
     }
 
-    public static int binarySearch(int[] A, int k) {
+    public static int binarySearchIterative(int[] A, int k) {
         int length = A.length;
         int left = 0;
         int right = length - 1;
@@ -24,5 +26,22 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    public static int binarySearchRecursive(int[] A, int k, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid =  left + (right - left) / 2;
+        if (A[mid] == k) {
+            return mid;
+        }
+
+        if (A[mid] < k)
+            return binarySearchRecursive(A, k, mid + 1, right);
+        else
+            return binarySearchRecursive(A, k, left, mid - 1);
+
     }
 }
