659ac427ed3838f00e7f572ae453dd51de64715d
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-05 17:23:38 +0530

    Add insert method for BinarySearchTree

---
+++ src/bst/BinarySearchTree.java
@@ -0,0 +1,28 @@
+package bst;
+
+public class BinarySearchTree {
+
+    BSTNode<Integer> root;
+
+    BinarySearchTree() {
+        this.root = null;
+    }
+
+    public BSTNode<Integer> insert(int value) {
+        root = insertRecursively(root, value);
+        return root;
+    }
+
+    private BSTNode<Integer> insertRecursively(BSTNode<Integer> node, int value) {
+        if (node == null) {
+            return new BSTNode<>(value);
+        }
+
+        if (value < node.getValue()) {
+            node.setLeft(insertRecursively(node.getLeft(), value));
+        } else {
+            node.setRight(insertRecursively(node.getRight(), value));
+        }
+        return node;
+    }
+}
