6db444dfac004c45f2f1288e050082f41cc27f0a
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-09 17:15:39 +0530

    Add NodeCount

--- src/trees/NodeCount.java
+++ src/trees/NodeCount.java
@@ -1,24 +1,65 @@
 package trees;
 /*
 * Given a complete Binary Tree, return number of nodes.
-*
-*
+*         1
+        /   \
+       2     3
+      / \   / \
+     4   5 6   7
+    /
+   8
+
+   answer = 8
+
 * */
 public class NodeCount {
     public static void main(String[] args) {
         BTNode<Integer> root = constructBT();
 
         System.out.println(getNodeCount1(root));
+        System.out.println(getNodeCount2(root));
 
     }
 
-    private static int getNodeCount1(BTNode<Integer> root) {
+    public static int getNodeCount1(BTNode<Integer> root) {
         if (root == null)
             return 0;
 
         return 1 + getNodeCount1(root.getLeft()) + getNodeCount1(root.getRight());
     }
 
+    public static int getNodeCount2(BTNode<Integer> root) {
+        if (root == null)
+            return 0;
+
+        int leftHeight = getLeftHeight(root.getLeft());
+        int rightHeight = getRightHeight(root.getRight());
+        if (leftHeight == rightHeight) {
+            return ((int) Math.pow(2, leftHeight + 1)) - 1 ;
+        }
+
+        return 1 + getNodeCount2(root.getLeft()) + getNodeCount2(root.getRight());
+    }
+
+    public static int getLeftHeight(BTNode<Integer> node) {
+        if (node == null) return 0;
+        int height = 1;
+        while (node.getLeft() != null) {
+            height += 1;
+            node = node.getLeft();
+        }
+        return height;
+    }
+
+    public static int getRightHeight(BTNode<Integer> node) {
+        if (node == null) return 0;
+        int height = 1;
+        while (node.getRight() != null) {
+            height += 1;
+            node = node.getRight();
+        }
+        return height;
+    }
 
 
 
