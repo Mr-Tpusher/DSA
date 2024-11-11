a74a06a5238d3f4c4772fedb6ba71bf642c4d61e
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-11 18:29:40 +0530

    Add ArrayTrie

---
+++ src/tries/ArrayTrieMain.java
@@ -0,0 +1,8 @@
+package tries;
+
+public class ArrayTrieMain {
+    public static void main(String[] args) {
+        ArrayTrie arrayTrie = new ArrayTrie();
+        arrayTrie.insert("cat");
+    }
+}
