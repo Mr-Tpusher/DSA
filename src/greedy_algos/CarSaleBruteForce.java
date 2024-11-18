735a6d2cf7ae7c5bc74c1b6d93e4c2eac5500eb5
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-18 14:32:35 +0530

    Add CarSale

--- src/greedy_algos/CarSaleBruteForce.java
+++ src/greedy_algos/CarSaleBruteForce.java
@@ -84,32 +84,4 @@ public class CarSaleBruteForce {
         }
         return saleEndTime;
     }
-
-}
-
-
-class Car {
-    int saleEndTime;
-    int beauty;
-
-    public Car(int a, int b) {
-        this.saleEndTime = a;
-        this.beauty = b;
-    }
-
-    public static List<Car> getCars(int[] A, int[] B) {
-        ArrayList<Car> cars = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            cars.add(new Car(A[i], B[i]));
-        }
-        return cars;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "saleEndTime=" + saleEndTime +
-                ", beauty=" + beauty +
-                '}';
-    }
 }
