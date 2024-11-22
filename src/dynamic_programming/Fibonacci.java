8bcb695755880e20c7b750148f242f4f20ee5d66
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-22 13:07:28 +0530

    Add Fibonacci series

--- src/dynamic_programming/Fibonacci.java
+++ src/dynamic_programming/Fibonacci.java
@@ -5,16 +5,28 @@ package dynamic_programming;
 * */
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 20;
 
         System.out.println(fibo1(n));
+        System.out.println(fibo2(n, new int[n+1]));
 
-        int[] fibos = new int[n+1];
-        System.out.println(fibo2(n, fibos));
+        System.out.println("bottomUp1:" + bottomUpFibo1(n));
+        System.out.println("bottomUp2:" + bottomUpFibo2(n));
 
 
     }
 
+    // A typical recursive top-down approach
+    // with TC: O(2^N) and SC:O(2^N)
+    public static int fibo1(int x) {
+        if (x < 2) {
+            return x;
+        }
+        return fibo1(x - 1) + fibo1(x - 2);
+    }
+
+    // Top-down with memoization
+    // TC: O(N) and SC:O(N)
     private static int fibo2(int n, int[] fibos) {
         if (n < 2) return n;
 
@@ -24,10 +36,30 @@ public class Fibonacci {
         return fibos[n];
     }
 
-    public static int fibo1(int x) {
-        if (x < 2) {
-            return x;
+
+    // Bottom-up approach with TC:O(N) and SC:O(N)
+    public static int bottomUpFibo1(int n) {
+        int[] output = new int[n + 1];
+        output[0] = 0;
+        output[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            if (output[i] != 0) {
+                return output[i];
+            }
+            output[i] = output[i - 1] + output[i - 2];
         }
-        return fibo1(x - 1) + fibo1(x - 2);
+        return output[n];
+    }
+
+    // Bottom-up without any extra space
+    public static int bottomUpFibo2(int n) {
+        int first = 0, second = 1;
+        for (int i = 2; i <= n; i++) {
+            int curr = first + second;
+            first = second;
+            second = curr;
+        }
+        return second;
     }
 }
