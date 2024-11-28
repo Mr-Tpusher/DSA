94caf80c4144b339a6da1287b3439a2cf6e4d32f
Mr-Tpusher <tech.tpusher@gmail.com>
2024-11-28 14:36:04 +0530

    Add BruteForce2 for CoinSum

--- src/dynamic_programming/CoinSum.java
+++ src/dynamic_programming/CoinSum.java
@@ -25,8 +25,10 @@ public class CoinSum {
         int[] A = {1, 3, 4};
         int R = 5;
         System.out.println(bruteForce(A, R));
+        System.out.println(bruteForce2(A, R));
     }
 
+    // Brute force approach where we calculate all the possible combinations
     public static int bruteForce(int[] A, int R) {
         ArrayList<ArrayList<Integer>> totalWays = new ArrayList<>();
         ArrayList<Integer> way = new ArrayList<>();
@@ -39,6 +41,22 @@ public class CoinSum {
         }
         return count;
     }
+
+    // Instead of calculating all combinations we just increment the count
+    public static int bruteForce2(int[] A, int R) {
+        if (R == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] <= R) {
+                count += bruteForce2(A, R - A[i]);
+            }
+        }
+        return count;
+    }
+
 
     public static void bruteForce(int[] A, int R, ArrayList<ArrayList<Integer>> totalWays,
                                   ArrayList<Integer> ways) {
