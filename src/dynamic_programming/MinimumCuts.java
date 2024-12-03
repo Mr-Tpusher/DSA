1b4d6eda2f2d88ace288168202d627754a01f6af
Mr-Tpusher <tech.tpusher@gmail.com>
2024-12-03 13:57:44 +0530

    Add MinimumCuts

--- src/dynamic_programming/MinimumCuts.java
+++ src/dynamic_programming/MinimumCuts.java
@@ -1,6 +1,7 @@
 package dynamic_programming;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  * Given a string s, find the minimum number of cuts required to make all the partitions
@@ -14,9 +15,67 @@ import java.util.ArrayList;
  * */
 public class MinimumCuts {
     public static void main(String[] args) {
-        String s = "abcdcab";
+        String s = "tbacdcab";
         //bruteForce(s);
         System.out.println(bruteForce(s));
+        System.out.println(getMinCuts(s));
+    }
+
+    private static int getMinCuts(String s) {
+        int length = s.length();
+       boolean[][] isPalindrome = getPalindromeMatrix(s);
+
+       //min number of cuts required between substring (i,j) to make strings palindrome
+        int[][] minCuts = new int[length][length];
+        for (int[] arr : minCuts) {
+            Arrays.fill(arr, Integer.MAX_VALUE);
+        }
+
+        return getMinCuts(s, 0, s.length() - 1, isPalindrome, minCuts);
+    }
+
+    private static int getMinCuts(String s, int startIndex, int endIndex, boolean[][] isPalindrome, int[][] minCuts) {
+
+        if (startIndex >= s.length() || isPalindrome[startIndex][endIndex]) {
+            return 0;
+        }
+
+        if (minCuts[startIndex][endIndex] != Integer.MAX_VALUE) {
+            return minCuts[startIndex][endIndex];
+        }
+
+        int currMinCuts = Integer.MAX_VALUE;
+
+        for (int currEndIndex = startIndex; currEndIndex < s.length(); currEndIndex++) {
+            if (isPalindrome[startIndex][currEndIndex]) {
+                currMinCuts = Math.min(currMinCuts, 1 + getMinCuts(s, currEndIndex + 1, endIndex, isPalindrome, minCuts));
+            }
+        }
+
+        minCuts[startIndex][endIndex] = currMinCuts;
+        return currMinCuts;
+
+    }
+
+    private static boolean[][] getPalindromeMatrix(String s) {
+        int length = s.length();
+        boolean[][] isPalindrome = new boolean[length][length];
+
+        for (int i = 0; i < length; i++) {
+            isPalindrome[i][i] = true;
+        }
+
+        for (int l = 2; l <= length; l++) {
+            for (int i = 0; i <= length - l; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
+                } else {
+                    isPalindrome[i][j] = false;
+                }
+            }
+        }
+        return isPalindrome;
     }
 
     public static int bruteForce(String s) {
