357f9663f8511441d3a5fa01e667de528da3d53c
Mr-Tpusher <tech.tpusher@gmail.com>
2024-12-03 19:32:43 +0530

    Add Recursive bruteforce for NDigitNumber

--- src/dynamic_programming/NDigitNumber.java
+++ src/dynamic_programming/NDigitNumber.java
@@ -6,10 +6,13 @@ package dynamic_programming;
 * */
 public class NDigitNumber {
     public static void main(String[] args) {
-        int N = 3;
-        int S = 4;
+        int N = 4;
+        int S = 25;
         System.out.println(bruteForce(N, S));
+        System.out.println(recursiveBruteForce(N, S));
     }
+
+
     /*
     * consider N = 2
     * N digit numbers 10 to 99 i.e. 10^(N-1) to 10^N - 1
@@ -36,4 +39,19 @@ public class NDigitNumber {
         }
         return count;
     }
+
+
+    public static int recursiveBruteForce(int N, int S) {
+        // Base cases
+        if (N <= 0) return 0;
+        if (N == 1 && S > 9) return 0;
+        if (N == 1 && S > 0 && S <= 9) return 1;
+
+        int count = 0;
+        for (int i = 0; i <= 9; i++) {
+            count += recursiveBruteForce(N - 1, S - i);
+        }
+        return count;
+    }
+
 }
