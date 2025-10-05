b2bf82354337947281ff9e8dbe730bf0d3de183e
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-05 13:52:39 +0530

    add all subarrays size wise

---
+++ src/revison_oct2025/arrays/AllSubArraysSizeWise.java
@@ -0,0 +1,27 @@
+package revison_oct2025.arrays;
+
+import java.util.Arrays;
+
+public class AllSubArraysSizeWise {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 11, 19};
+        allSubArraysSizeWise(arr);
+
+    }
+
+    public static void allSubArraysSizeWise(int[] arr) {
+
+        int[][] allSubarrays = new int[arr.length * (arr.length + 1) / 2][];
+
+        for (int size = 1, x = 0; size <= arr.length; size++) {
+            for (int i = 0; i + size <= arr.length; i++, x++) {
+                int[] subArr = new int[size];
+                for (int j = i, z = 0; (j < i + size); j++, z++) {
+                    subArr[z] = arr[j];
+                }
+                allSubarrays[x] = subArr;
+            }
+        }
+        System.out.println(Arrays.deepToString(allSubarrays));
+    }
+}
