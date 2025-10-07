5747ebfe51ae1cb7fd0d6dd62b4a921197116cf7
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-07 16:23:15 +0530

    brute force for rectangle configs

---
+++ src/revison_oct2025/two_pointers/RectangleConfigs.java
@@ -0,0 +1,41 @@
+package revison_oct2025.two_pointers;
+
+import java.util.Arrays;
+
+/*
+* Given a sorted array, return count of different configurations of rectangle,
+* such that the area of those rectangles <= B.
+* arr = {2, 3, 5} , B = 10
+*
+* ans:
+* {2,2} {2,3} {2,5} {3,2} {3,3} {5,2} = 6
+*
+*
+* */
+public class RectangleConfigs {
+    public static void main(String[] args) {
+        test(new int[]{2, 3, 5}, 10, 6);
+        test(new int[]{1, 2, 3, 5, 10}, 10, 15);
+        test(new int[]{11,12,13,14,15,17}, 10, 0);
+
+
+    }
+
+    static void test(int[] arr, int B, int expected) {
+        System.out.println("arr = " + Arrays.toString(arr) + ", B = " + B);
+        System.out.println("expected = " + expected);
+        System.out.println("actual = " + bruteForce(arr, B));
+    }
+
+    static int bruteForce(int[] arr, int B) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] * arr[j] <= B) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
