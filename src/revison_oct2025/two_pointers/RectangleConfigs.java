483a328c77ab04bb5f95e3f0f1800300df2dea22
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-07 17:19:07 +0530

    add rectangle configs

--- src/revison_oct2025/two_pointers/RectangleConfigs.java
+++ src/revison_oct2025/two_pointers/RectangleConfigs.java
@@ -3,20 +3,20 @@ package revison_oct2025.two_pointers;
 import java.util.Arrays;
 
 /*
-* Given a sorted array, return count of different configurations of rectangle,
-* such that the area of those rectangles <= B.
-* arr = {2, 3, 5} , B = 10
-*
-* ans:
-* {2,2} {2,3} {2,5} {3,2} {3,3} {5,2} = 6
-*
-*
-* */
+ * Given a sorted array, return count of different configurations of rectangle,
+ * such that the area of those rectangles <= B.
+ * arr = {2, 3, 5} , B = 10
+ *
+ * ans:
+ * {2,2} {2,3} {2,5} {3,2} {3,3} {5,2} = 6
+ *
+ *
+ * */
 public class RectangleConfigs {
     public static void main(String[] args) {
         test(new int[]{2, 3, 5}, 10, 6);
         test(new int[]{1, 2, 3, 5, 10}, 10, 15);
-        test(new int[]{11,12,13,14,15,17}, 10, 0);
+        test(new int[]{11, 12, 13, 14, 15, 17}, 10, 0);
 
 
     }
@@ -24,7 +24,9 @@ public class RectangleConfigs {
     static void test(int[] arr, int B, int expected) {
         System.out.println("arr = " + Arrays.toString(arr) + ", B = " + B);
         System.out.println("expected = " + expected);
-        System.out.println("actual = " + bruteForce(arr, B));
+        // int ans = bruteForce(arr, B);
+        int ans = solve(arr, B);
+        System.out.println("actual = " + ans);
     }
 
     static int bruteForce(int[] arr, int B) {
@@ -35,6 +37,22 @@ public class RectangleConfigs {
                     count++;
                 }
             }
+        }
+        return count;
+    }
+
+    static int solve(int[] arr, int B) {
+        int i = 0, j = arr.length - 1, count = 0;
+
+        while (i <= j) {
+            int area = arr[i] * arr[j];
+            if (area <= B) {
+                count = count + (((j - i) * 2) + 1);
+                i++;
+            } else {
+                j--;
+            }
+
         }
         return count;
     }
