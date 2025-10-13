50af80005c1f2983da3629eb0bc5f718b40b0481
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-13 13:36:06 +0530

    modify the MyStack

---
+++ src/revison_oct2025/stack/MyStackDemo.java
@@ -0,0 +1,19 @@
+package revison_oct2025.stack;
+
+public class MyStackDemo {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack.push(1));
+        myStack.push(2);
+        myStack.push(3);
+        System.out.println(myStack);
+        System.out.println("current top = " + myStack.top());
+        System.out.println("popped = " + myStack.pop());
+        System.out.println("stack : " + myStack);
+        System.out.println("popped = " + myStack.pop());
+        System.out.println("stack : " + myStack);
+        myStack.push(4);
+        System.out.println(myStack);
+    }
+}
