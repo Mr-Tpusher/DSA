bb9f1ae9745b7dbd568bf6935ce0680c79050378
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-17 01:45:56 +0530

    add LinkedListUtil - add method printLL()

---
+++ src/revison_oct2025/linkedlists/LinkedListUtil.java
@@ -0,0 +1,13 @@
+package revison_oct2025.linkedlists;
+
+public class LinkedListUtil {
+    static void printLL(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null)
+                System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
