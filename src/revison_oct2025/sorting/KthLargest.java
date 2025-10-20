df74c7621058696894e640453f92c8331edc7967
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-20 16:47:28 +0530

    sorting - Kth Largest number in an array

---
+++ src/revison_oct2025/sorting/KthLargest.java
@@ -0,0 +1,27 @@
+package revison_oct2025.sorting;
+
+public class KthLargest {
+    public static void main(String[] args) {
+        int[] A = {10, 5, 9, 8, 2, 3, 7, 6, 1, 4};
+        int k = 3;
+        System.out.println(kthLargest(A, k));
+    }
+
+    static int kthLargest(int[] A, int k) {
+        if (k > A.length) return 0;
+        for (int i = 0; i < k; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[j] > A[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            if (maxIndex != i) {
+                int temp = A[maxIndex];
+                A[maxIndex] = A[i];
+                A[i] = temp;
+            }
+        }
+        return A[k - 1];
+    }
+}
