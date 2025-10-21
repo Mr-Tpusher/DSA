6c5234899f05fd8243489d667b9e8782bb92f4cf
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-21 13:24:54 +0530

    trees - BTNode

---
+++ src/revison_oct2025/trees/BTNode.java
@@ -0,0 +1,12 @@
+package revison_oct2025.trees;
+
+public class BTNode {
+    int val;
+    BTNode left;
+    BTNode right;
+    public BTNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
