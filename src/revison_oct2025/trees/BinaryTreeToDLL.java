8687fb2d7e019d883cc933c02de2b129bae510da
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-24 14:39:40 +0530

    tree - Binary tree to DLL

---
+++ src/revison_oct2025/trees/BinaryTreeToDLL.java
@@ -0,0 +1,38 @@
+package revison_oct2025.trees;
+
+public class BinaryTreeToDLL {
+    BTNode head = null,prev = null;
+    public static void main(String[] args) {
+        BTNode root = BinaryTree.buildTree(new int[] {1, 2, 3, 4, 5, -1, 6});
+        System.out.println(BinaryTree.inOrder(root));
+
+        BinaryTreeToDLL btd = new BinaryTreeToDLL();
+        btd.binaryTreeToDLL(root);
+        printDLL(btd.head);
+
+    }
+
+    void binaryTreeToDLL(BTNode node) {
+        if (node == null)
+            return;
+
+        binaryTreeToDLL(node.left);
+        if (prev == null) {
+            head = node;
+        } else {
+            prev.right = node;
+            node.left = prev;
+        }
+        prev = node;
+        binaryTreeToDLL(node.right);
+    }
+
+
+    static void printDLL(BTNode node) {
+        while (node != null) {
+            System.out.print(node.val + " <--> ");
+            node = node.right;
+        }
+        System.out.print("null");
+    }
+}
