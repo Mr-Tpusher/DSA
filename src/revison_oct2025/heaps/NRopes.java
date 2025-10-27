eaf9eca86adb966e4576f30ff23c8fb24f59f4d0
Mr-Tpusher <tech.tpusher@gmail.com>
2025-10-27 18:25:06 +0530

    heaps - N Ropes

---
+++ src/revison_oct2025/heaps/NRopes.java
@@ -0,0 +1,27 @@
+package revison_oct2025.heaps;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class NRopes {
+    public static void main(String[] args) {
+        int[] ropes = new int[] {16,7,3,5,9,8,6,15};
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i : ropes)
+            pq.offer(i);
+
+        int cost = 0;
+        while (pq.size() != 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            int knot = first + second;
+            cost += knot;
+            pq.offer(knot);
+        }
+
+        System.out.println(cost);
+    }
+}
