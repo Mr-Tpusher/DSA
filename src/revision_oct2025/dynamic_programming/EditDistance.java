67d68aa2a9ddbf2a2334bb675c59cc5105694602
Mr-Tpusher <tech.tpusher@gmail.com>
2025-11-03 16:16:44 +0530

    dp -> edit distance (top down)

--- src/revision_oct2025/dynamic_programming/EditDistance.java
+++ src/revision_oct2025/dynamic_programming/EditDistance.java
@@ -1,4 +1,9 @@
 package revision_oct2025.dynamic_programming;
+
+import dsa_2024_25.heaps.MaxHeap;
+
+import java.util.Arrays;
+
 /*
 * Given 2 strings, convert String A to String B, only 3 operations are allowed:
 * 1. insert a char
@@ -14,11 +19,52 @@ public class EditDistance {
         String B = "antihuman";
 
         System.out.println(bruteForce(A, B));
+        System.out.println(editDistanceTopDownDP(A, B));
 
     }
 
     static int bruteForce(String A, String B) {
         return bruteForceHelper(A, B, 0, 0);
+    }
+
+    static int editDistanceTopDownDP(String A, String B) {
+
+        int[][] dp = new int[A.length() + 1][B.length() + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return topDownDPHelper(A, B, 0, 0, dp);
+    }
+
+    static int topDownDPHelper(String A, String B, int indexA, int indexB, int[][] dp) {
+        // when source is completely traversed
+        if (indexA == A.length()) {
+            dp[indexA][indexB] = B.length() - indexB;
+            return dp[indexA][indexB];
+        }
+
+        // when destination is completely traversed
+        if (indexB == B.length()) {
+            dp[indexA][indexB] = A.length() - indexA;
+            return dp[indexA][indexB];
+        }
+
+        // return memoized result
+        if (dp[indexA][indexB] != -1) {
+            return dp[indexA][indexB];
+        }
+
+        // if current char match
+        if (A.charAt(indexA) == B.charAt(indexB)) {
+            dp[indexA][indexB] =  topDownDPHelper(A, B, indexA + 1, indexB + 1, dp);
+        } else {
+            int replace = 1 + topDownDPHelper(A, B, indexA + 1, indexB + 1, dp);
+            int insert = 1 + topDownDPHelper(A, B, indexA, indexB + 1, dp);
+            int delete = 1 + topDownDPHelper(A, B, indexA + 1, indexB, dp);
+            dp[indexA][indexB] = Math.min(Math.min(replace, insert), delete);
+        }
+
+        return dp[indexA][indexB];
     }
 
     static int bruteForceHelper(String A, String B, int indexA, int indexB) {
