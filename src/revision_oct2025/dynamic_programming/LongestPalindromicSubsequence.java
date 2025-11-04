d7f79f68c3e164442810cd144796cf158c206477
Mr-Tpusher <tech.tpusher@gmail.com>
2025-11-04 21:00:43 +0530

    dp - longest palindromic subsequence (bottom up)

--- src/revision_oct2025/dynamic_programming/LongestPalindromicSubsequence.java
+++ src/revision_oct2025/dynamic_programming/LongestPalindromicSubsequence.java
@@ -12,6 +12,7 @@ public class LongestPalindromicSubsequence {
         String s = "agbdba";
         System.out.println(bruteForce(s));
         System.out.println(topDownDP(s));
+        System.out.println(bottomUpDP(s));
 
     }
 
@@ -30,6 +31,26 @@ public class LongestPalindromicSubsequence {
 
         return dp[0][s.length() - 1];
 
+    }
+
+    static int bottomUpDP(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][n + 1];
+
+        // pre-populate the base cases
+        for (int i = 1; i <= n; i++)
+            dp[i][i] = 1;
+
+        for (int i = n; i > 0; i--) {
+            for (int j = i + 1; j <= n; j++) { // j is i + 1 handles base case of i > j
+                if (s.charAt(i - 1) == s.charAt(j - 1)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[1][n];
     }
 
     static int topDownHelper(String s, int left, int right, int[][] dp) {
